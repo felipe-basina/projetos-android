df1e361c8055c84f7e25e5bbbb149015c44a6139
felipe <felipe.basina@gmail.com>
2017-03-06 22:31:49 -0300

    Alterado layout para exibir a tela em abas; ajuste da activity para controle do novo layout

--- RestauranteApp/app/src/main/java/br/com/diversos/exemplos/restauranteapp/RestaurantesActivity.java
+++ RestauranteApp/app/src/main/java/br/com/diversos/exemplos/restauranteapp/RestaurantesActivity.java
@@ -1,17 +1,22 @@
 package br.com.diversos.exemplos.restauranteapp;
 
+import android.app.TabActivity;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,12 +29,20 @@ import br.com.diversos.exemplos.restauranteapp.modelo.Restaurante;
 /**
  * Referência: blog http://www.rafaeltoledo.net/
  ***/
-// Continuar a partir de http://www.rafaeltoledo.net/tutorial-android-6-utilizando-abas/
-public class RestaurantesActivity extends AppCompatActivity {
+// Continuar a partir de http://www.rafaeltoledo.net/tutorial-android-7-menus-e-mensagens/
+public class RestaurantesActivity extends TabActivity {
 
     private List<Restaurante> listaRestaurantes = new ArrayList<Restaurante>();
     private AdaptadorRestaurante adaptador = null;
     private static Map<String, Integer> iconeMapa = new HashMap<String, Integer>();
+
+    // Valores para edição
+    private EditText nome = null;
+    private EditText endereco = null;
+    private RadioGroup tipos = null;
+
+    private static int ABA_LISTAGEM = 0;
+    private static int ABA_DETALHES = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +55,67 @@ public class RestaurantesActivity extends AppCompatActivity {
         Button salvar = (Button) findViewById(R.id.salvar);
         salvar.setOnClickListener(onSave); // Adiciona um listener para o evento clicar
 
+        Button limpar = (Button) findViewById(R.id.limpar);
+        limpar.setOnClickListener(onClear);
+
         ListView lista = (ListView) findViewById(R.id.restaurantes);
         adaptador = new AdaptadorRestaurante();
         lista.setAdapter(adaptador);
+        lista.setOnItemClickListener(onItemClickListener); // Adiciona o listener para edição de registros
 
         // Define um mapa com os valores referentes aos ícones
         iconeMapa.put("rodizio", R.drawable.rodizio);
         iconeMapa.put("fast_food", R.drawable.fast_food);
         iconeMapa.put("a_domicilio", R.drawable.entrega);
+
+        // Define os atributos para edição
+        nome = (EditText) findViewById(R.id.nome);
+        endereco = (EditText) findViewById(R.id.end);
+        tipos = (RadioGroup) findViewById(R.id.tipos);
+
+        // Controle das abas
+        TabHost.TabSpec descritor = getTabHost().newTabSpec("tag1");
+        descritor.setContent(R.id.restaurantes);
+        Drawable d1 = ContextCompat.getDrawable(this, R.drawable.lista);
+        descritor.setIndicator("Lista", d1);
+        getTabHost().addTab(descritor);
+
+        descritor = getTabHost().newTabSpec("tag2");
+        descritor.setContent(R.id.detalhes);
+
+        Drawable d2 = ContextCompat.getDrawable(this, R.drawable.restaurante);
+        descritor.setIndicator("Detalhes", d2);
+        getTabHost().addTab(descritor);
+
+        getTabHost().setCurrentTab(ABA_LISTAGEM); // Define a aba inicial
     }
+
+    private AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // Recupera o registro selecionado na lista
+            Restaurante selecionado = listaRestaurantes.get(position);
+            nome.setText(selecionado.getNome());
+            endereco.setText(selecionado.getEndereco());
+
+            if (selecionado.getTipo().equalsIgnoreCase("rodizio")) {
+                tipos.check(R.id.rodizio);
+            } else if (selecionado.getTipo().equalsIgnoreCase("fast_food")) {
+                tipos.check(R.id.fast_food);
+            } else {
+                tipos.check(R.id.a_domicilio);
+            }
+
+            getTabHost().setCurrentTab(ABA_DETALHES);
+        }
+    };
+
+    private View.OnClickListener onClear = new View.OnClickListener() {
+        public void onClick(View arg0) {
+            // Limpa os valores do formulário
+            limpar();
+        }
+    };
 
     private View.OnClickListener onSave = new View.OnClickListener() {
 
@@ -64,6 +129,19 @@ public class RestaurantesActivity extends AppCompatActivity {
             // Recupera valores da tela
             nome = (EditText) findViewById(R.id.nome);
             endereco = (EditText) findViewById(R.id.end);
+
+            /**
+             * Caso o nome a ser salvo contenha valores que existam na listagem,
+             * remove o registro da listagem e cadastra o novo valor
+             * Caso contrário, permite adicionar um novo item na lista
+             **/
+            for (int restauranteIndex = 0; restauranteIndex < adaptador.getCount(); restauranteIndex++) {
+                Restaurante existente = (Restaurante) adaptador.getItem(restauranteIndex);
+                if (nome.getText().toString().toUpperCase().contains(existente.getNome().toUpperCase())) {
+                    listaRestaurantes.remove(restauranteIndex);
+                    break;
+                }
+            }
 
             // Define os valores recuperados da tela no objeto 'restaurante'
             r.setNome(nome.getText().toString());
@@ -86,16 +164,19 @@ public class RestaurantesActivity extends AppCompatActivity {
             adaptador.add(r);
 
             // Limpa os valores do formulário
-            this.limpar();
-        }
+            limpar();
 
-        public void limpar() {
-            nome.setText("");
-            nome.requestFocus(); // Define o cursor
-            endereco.setText("");
-            tipos.clearCheck();
+            // Exibe a aba de listagem
+            getTabHost().setCurrentTab(ABA_LISTAGEM);
         }
     };
+
+    private void limpar() {
+        nome.setText("");
+        nome.requestFocus(); // Define o cursor
+        endereco.setText("");
+        tipos.clearCheck();
+    }
 
     class AdaptadorRestaurante extends ArrayAdapter {
         AdaptadorRestaurante() {
